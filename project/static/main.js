b4fa3df9c92cbbbd68bac7a5a4a226ee6453161a
Sidorov Pavel <sidorovpavel564@gmail.com>
2023-04-23 19:07:39 +0300

    h

--- project/static/main.js
+++ project/static/main.js
@@ -40,43 +40,23 @@ function getAllProducts(url) {
     .then(response => response.json())
     .then(data => {
         const productList = document.getElementById("productList")
-        productList.innerHTML = "";
+        productList.innerHTML = `<thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">product_name</th>
+                    <th scope="col">quantity</th>
+                </tr>
+            </thead>`;
 
         (data.context).forEach(product => {
             const productHTMLElement = `
-                <div class="card rounded-3 shadow-5 mb-4">
-                    <div class="card-body p-4">
-                        <div class="row d-flex justify-content-between align-items-center">
-                            <div class="col-md-2 col-lg-2 col-xl-2">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                                    class="img-fluid rounded-3" alt="Cotton T-shirt">
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-3">
-                                <p class="lead fw-normal mb-2">${product.product_name}</p>
-                                <p><span class="text-muted">Size: </span>M <span class="text-muted">Color: </span>Grey</p>
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                
-                                <input id="form1" min="0" name="quantity" value="${product.quantity}" type="number"
-                                    class="form-control form-control-sm" />
-                
-                                <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                    <i class="fas fa-plus"></i>
-                                </button>
-                            </div>
-                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                <h5 class="mb-0">$499.00</h5>
-                            </div>
-                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <!-- <button id="x-button-delete" product-id="${product.id}" class="btn btn-primary">asdasd</button> -->
-                                <a id="x-button-delete" product-id="${product.id}" role="button" class="text-danger"><i class="fas fa-xmark fa-lg"></i></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>`
+                <tbody>
+                    <tr>
+                        <th scope="row">${product.id}</th>
+                        <td>${product.product_name}</td>
+                        <td>${product.quantity}</td>
+                    </tr>
+                </tbody>`;
             productList.innerHTML += productHTMLElement;
         });
     });
